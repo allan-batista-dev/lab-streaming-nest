f492c2d93367b57b1d5cf7f4ca956fcce370d7a2
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-09-03 10:31:17 -0300

    auth end user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    PrismaService,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedErrorInterceptor } from './common/errors/interceptors/unauthorizes.interception';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  })
+  );
+  app.useGlobalInterceptors(new UnauthorizedErrorInterceptor);
+  app.useGlobalInterceptors(new NotFoundInterceptor);
+  await app.listen(3333);
 }
 bootstrap();
